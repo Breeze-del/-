0147b8cec102c0f5e49b1562f2704b321fe7131a
Liqingfeng <562916584@qq.com>
2018-11-12 13:24:58 +0800

    修改完成

--- wordcloud/spread.py
+++ wordcloud/spread.py
@@ -1,2 +1,12 @@
 # encoding=utf-8
-import
+import jieba
+import wordcloud
+# 从文件读入信息
+def read(filname):
+    f = open(filname,"r",encoding="utf-8")
+    lines = f.readlines()
+    for line in lines:
+        a=Item()
+        line.split(" ")
+if __name__=="__main__":
+    read("res.txt")
--- wordcloud/wordCloud_main.py
+++ wordcloud/wordCloud_main.py
@@ -1,5 +1,9 @@
 # encoding=utf-8
 import json
+from wordcloud import WordCloud,STOPWORDS,ImageColorGenerator
+import jieba
+import pickle
+import matplotlib.pyplot as plt
 class item:
     def __init__(self):
         self.id=0 # 店铺id
@@ -30,15 +34,58 @@ def deal(ls):
                 ls[j].flag=True
                 ls[i].count+=1
     return ls
+# 将结果写入文件
 def output(ls):
-    w =open("res.txt","w",encoding="utf-8")
-    w.write("itemId     "+"count     "+"review     ")
-    for v in ls :
-        w.write(v.id+"     "+str(v.count)+"     "+v.review+"     \n")
+    str = ""
+    for v in ls:
+        str += v.review
+    # w =open("res.txt","w",encoding="utf-8")
+    # w.write("itemId     "+"count     "+"review     ")
+    # for v in ls :
+    #     w.write(v.id+"     "+str(v.count)+"     "+v.review+"     \n")
+    # w.close()
+    w = open("word.txt","w",encoding="utf-8")
+    w.write(str)
     w.close()
+# 词云分析
+def cloud():
+    fr = open('word.txt', 'r',encoding="utf-8")
+    text = fr.read()
+    fr.close()
+    # jieba分词
+    removes = ['团购', '点评', '但是', '还是', '感觉', '就是', '而且', '没有', '还有', '不过', '知道']
+    for w in removes:
+        jieba.del_word(w)
+    words = jieba.lcut(text)
+    words = [w for w in words if w not in removes]
+    cuted = ' '.join(words)
+    # wordCloud 生成词云
+    fontpath = "SourceHanSansCN-Regular.otf"
+    #backgroud_Image = plt.imread('cloud.jpg')
+    wc = WordCloud(background_color='white',  # 设置背景颜色
+                   #mask=backgroud_Image,  # 设置背景图片
+                   max_words=100,  # 设置最大现实的字数
+                   stopwords=STOPWORDS,  # 设置停用词
+                   font_path=fontpath,  # 设置字体格式，如不设置显示不了中文
+                   max_font_size=500,  # 设置字体最大值
+                   min_font_size=30,  # 设置字体最小值
+                   random_state=42,  # 设置有多少种随机生成状态，即有多少种配色方案
+                   collocations=False, # 避免重复的单词
+                   width=1600, height=1200,margin=10, # 设置图像宽高，字体间距
+                   )
+    wc.generate(cuted)
+    # image_colors = ImageColorGenerator(backgroud_Image)
+    # wc.recolor(color_func=image_colors)
+    plt.figure(dpi=100)
+    plt.imshow(wc, interpolation='catrom', vmax=1000)
+    plt.axis('off')
+    plt.savefig("zongpingfen1.jpg")
+    plt.show()
 if __name__ == "__main__":
-   ls=load()
-   ls=deal(ls)
-   output(ls)
-   for v in ls:
-       print(v.id, v.review)
+    cloud()
+   # ls=load()
+   # ls=deal(ls)
+   # output(ls)
+   # for v in ls:
+   #     print(v.id, v.review)
+
